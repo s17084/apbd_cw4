5a4ae7cea916b4035d0a82d6bcd14a1ee45562b1
s17084 <s17084@pjwstk.edu.pl>
2020-04-04 14:33:21 +0200

    Dodanie do repo kodu z Ćwiczenia 3

---
+++ Cwiczenia3/Controllers/StudentsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Cwiczenia3.DAL;
+using Cwiczenia3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenia3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudent(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if(id ==1)
+            {
+                return Ok("Kulig");
+            }else if (id == 2)
+            {
+                return Ok("Rychlik");
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id)
+        {
+            return Ok("Aktualizacja dokończona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Usuwanie ukończone");
+        }
+
+    }
+}
